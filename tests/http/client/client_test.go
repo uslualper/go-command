bca0b0c5e2399096f12f65b49cbb17dfa5b16daa
Alper <uslualper@outlook.com>
2024-03-05 09:46:25 +0300

    Test typo

--- tests/http/client/client_test.go
+++ tests/http/client/client_test.go
@@ -4,14 +4,15 @@ import (
 	"testing"
 
 	"go-command/pkg/utils/http"
+
+	"github.com/valyala/fasthttp"
 )
 
 func TestClient(t *testing.T) {
 
-	client := http.Client{}
-	client.Init("https://www.google.com")
+	client := http.NewClient(&fasthttp.Client{})
 	client.SetTimeout(1)
-	_, status := client.Get()
+	_, status := client.Get("https://www.google.com")
 
 	if status != 200 {
 		t.Errorf("Expected status 200, got %d", status)
